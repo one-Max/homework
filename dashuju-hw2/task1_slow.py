40718d879ab96bb1c6ab5010b01a2e01efeb3a77
zjw-server <2679794336@qq.com>
2022-11-08 20:11:59 +0800

    大数据作业2

---
+++ dashuju-hw2/task1_slow.py
@@ -0,0 +1,42 @@
+import sys, os, math, time, copy
+import matplotlib.pyplot as plt
+import numpy as np
+
+# ==================================================
+# 载入矩阵数据
+X_train = np.load('/data1/zjw/homework/dashuju-hw2/data/x_train.npy')
+X_test = np.load('/data1/zjw/homework/dashuju-hw2/data/x_test.npy')
+
+t0 = time.perf_counter()
+
+# ==================================================
+# 协同过滤生成预测值
+norm = np.sqrt(np.sum(X_train * X_train, axis=1)).reshape(-1, 1)
+norm_matrix = norm @ norm.T
+X1 = X_train @ X_train.T
+cos_matrix = X1 / norm_matrix
+weight = cos_matrix / np.sum(cos_matrix, axis=1).reshape(-1, 1)
+
+# X_pred = np.dot(weight, X_train)
+X_pred = np.zeros((10000, 10000))
+# ==================================================
+# 计算RMSE:只对那些X_test中非0的(非空)数据计算损失
+t1 = time.perf_counter()
+
+
+RMSE = 0
+rows, cols = np.nonzero(X_test)
+for row, col in zip(list(rows), list(cols)):
+    X_pred[row][col] = weight[row,:] @ X_train[:,col]
+    RMSE += (X_pred[row][col] - X_test[row][col]) ** 2
+RMSE = np.sqrt(RMSE / len(rows))
+
+t2 = time.perf_counter()
+
+print(f'RMSE: {RMSE:.8f}')
+print(f'run time pred: {t1 - t0:.8f}s')
+print(f'run time loss: {t2 - t1:.8f}s')
+
+
+# plt.spy(X_test)
+# plt.savefig('/data1/zjw/homework/0.png')
---
+++ dashuju-hw2/task2_self_define.py
@@ -0,0 +1,99 @@
+import sys, os, math, time, copy
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+os.system('echo $CUDA_VISIBLE_DEVICES')
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+torch.manual_seed(0)
+
+class UV(nn.Module):
+    def __init__(self, user_dim, film_dim, k):
+        super().__init__()
+        self.user_dim = user_dim
+        self.film_dim = film_dim
+        self.k = k
+        self.U = nn.Parameter(torch.rand(self.user_dim, self.k))
+        self.V = nn.Parameter(torch.rand(self.film_dim, self.k))
+    
+    def forward(self):
+        X_pred = torch.matmul(self.U, self.V.T)
+        return X_pred
+
+    def print_network(self):
+        num_params = 0
+        for param in self.parameters():
+            # print(param)
+            num_params += param.numel()
+        print('Total number of parameters: %d' % num_params)
+
+class MyLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, X_pred, X_gt, U, V, A, lam, lenth):
+        match_loss =   0.5 * torch.sum(torch.pow((torch.mul(A, (X_gt-X_pred))), 2)) 
+        norm_loss =  lam *(torch.sum(torch.pow(U, 2)) + torch.sum(torch.pow(V, 2)))
+        return match_loss + norm_loss
+
+
+def main():
+    # ==================================================
+    # 载入矩阵数据
+    X_train = torch.tensor(
+        np.load('/data1/zjw/homework/dashuju-hw2/data/x_train.npy'), dtype=torch.float32).cuda()
+    X_test = np.load('/data1/zjw/homework/dashuju-hw2/data/x_test.npy')
+
+    # ==================================================
+    # 参数初始化设置
+    user_dim = 10000; film_dim = 10000
+    k = 50
+    lr = 0.0001
+    num_epochs = 2000
+    lam = 0.01
+ 
+    # ==================================================
+    # 初始化UV参数
+    uv = UV(user_dim, film_dim, k).cuda()
+    uv.print_network()
+
+    rows, cols = np.nonzero(X_test)
+    A = np.zeros((10000, 10000))
+    A[rows, cols] = 1
+    lenth = len(rows)
+    A = torch.tensor(A, dtype=torch.float32).cuda()
+
+    parameters = uv.parameters()
+    loss_fn = MyLoss()
+    optimizer = torch.optim.SGD(parameters, lr=lr, momentum=0.9)
+    
+    # ==================================================
+    # 梯度下降得到U， V
+    loss_history = []
+    for epoch in range(num_epochs):
+        optimizer.zero_grad()
+        X_pred = uv.forward()
+        loss = loss_fn(X_pred, X_train, uv.U, uv.V, A, lam, lenth)
+        loss.backward()
+        optimizer.step()
+        loss_history.append(loss.item())
+        if (epoch+1) % 5 == 0:
+            print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch +
+              1, num_epochs, loss.item()))
+
+    # ==================================================
+    # 在测试集上得到RMSE
+    predicted = uv.forward().cpu().detach().numpy()
+    
+    RMSE = 0
+    for row, col in zip(list(rows), list(cols)):
+        RMSE += (predicted[row][col] - X_test[row][col]) ** 2
+    RMSE = (RMSE / len(rows)) ** 0.5
+    print(RMSE)
+
+
+if __name__ == '__main__':
+    main()
